364645c0e293cbed4cbdbe283e114826ead93baa
Fynn Schapdick <fynn@wechselpilot.com>
2025-05-06 19:56:55 +0200

    update nuget-publish.yml add markdown export for benchmarks

--- benchmarks/SteamId.Net.Benchmark/Program.cs
+++ benchmarks/SteamId.Net.Benchmark/Program.cs
@@ -1,4 +1,13 @@
-﻿using BenchmarkDotNet.Running;
+﻿using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Exporters;
+using BenchmarkDotNet.Loggers;
+using BenchmarkDotNet.Reports;
+using BenchmarkDotNet.Running;
 using SteamId.Net.Benchmark;
 
-BenchmarkRunner.Run<SteamIdBenchmark>();
+ManualConfig config = ManualConfig.Create(DefaultConfig.Instance)
+    .WithSummaryStyle(SummaryStyle.Default)
+    .AddLogger(ConsoleLogger.Default)
+    .AddExporter(MarkdownExporter.Default);
+
+BenchmarkRunner.Run<SteamIdBenchmark>(config);
